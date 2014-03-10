5aca5b216ccfb9aaa80c388f57a04db6442bff96
omar002 <y0028613702420400@gmail.com>
2014-03-10 19:20:11 +0800

    add unit test

--- kernel.c
+++ kernel.c
@@ -3,13 +3,13 @@
 #include "core_cm3.h"
 #include "syscall.h"
 #include <stddef.h>
+#include "kernel.h"
 
-typedef void (*task_entry_t)();
-
-void *memcpy(void *dest, const void *src, size_t n);
-void check_keyword();
-void find_events();
-void itoa(int n, char *dst, int base);
+void *malloc(size_t size)
+{
+	static char m[1024] = {0};
+	return m;
+}
 
 int strcmp(const char *a, const char *b) __attribute__ ((naked));
 int strcmp(const char *a, const char *b)
@@ -64,37 +64,6 @@ void puts(char *s)
 	}
 }
 
-#define MAX_CMDNAME 19
-#define MAX_ARGC 19
-#define MAX_CMDHELP 1023
-#define HISTORY_COUNT 20
-#define CMDBUF_SIZE 100
-#define MAX_ENVCOUNT 30
-#define MAX_ENVNAME 15
-#define MAX_ENVVALUE 127
-#define STACK_SIZE 512 /* Size of task stacks in words */
-#define TASK_LIMIT 8  /* Max number of tasks we can handle */
-#define PIPE_BUF   64 /* Size of largest atomic pipe message */
-#define PATH_MAX   32 /* Longest absolute path */
-#define PIPE_LIMIT (TASK_LIMIT * 2)
-
-#define PATHSERVER_FD (TASK_LIMIT + 3) 
-	/* File descriptor of pipe to pathserver */
-
-#define PRIORITY_DEFAULT 20
-#define PRIORITY_LIMIT (PRIORITY_DEFAULT * 2 - 1)
-
-#define TASK_READY      0
-#define TASK_WAIT_READ  1
-#define TASK_WAIT_WRITE 2
-#define TASK_WAIT_INTR  3
-#define TASK_WAIT_TIME  4
-
-#define S_IFIFO 1
-#define S_IMSGQ 2
-
-#define O_CREAT 4
-
 /*Global Variables*/
 char next_line[3] = {'\n','\r','\0'};
 size_t task_count = 0;
@@ -103,31 +72,9 @@ int cur_his=0;
 int fdout;
 int fdin;
 
-/* Command handlers. */
-void export_envvar(int argc, char *argv[]);
-void show_echo(int argc, char *argv[]);
-void show_cmd_info(int argc, char *argv[]);
-void show_task_info(int argc, char *argv[]);
-void show_man_page(int argc, char *argv[]);
-void show_history(int argc, char *argv[]);
-void create_process(int argc, char *argv[]);
-/* Enumeration for command types. */
-enum {
-	CMD_ECHO = 0,
-	CMD_EXPORT,
-	CMD_HELP,
-	CMD_HISTORY,
-	CMD_MAN,
-	CMD_PS,
-	CMD_EXEC,
-	CMD_COUNT
-} CMD_TYPE;
-/* Structure for command handler. */
-typedef struct {
-	char cmd[MAX_CMDNAME + 1];
-	void (*func)(int, char**);
-	char description[MAX_CMDHELP + 1];
-} hcmd_entry;
+evar_entry env_var[MAX_ENVCOUNT];
+int env_count = 0;
+
 const hcmd_entry cmd_data[CMD_COUNT] = {
 	[CMD_ECHO] = {.cmd = "echo", .func = show_echo, .description = "Show words you input."},
 	[CMD_EXPORT] = {.cmd = "export", .func = export_envvar, .description = "Export environment variables."},
@@ -138,46 +85,6 @@ const hcmd_entry cmd_data[CMD_COUNT] = {
 	[CMD_EXEC] = {.cmd = "exec", .func = create_process, .description = "Create a process dynamically"}
 };
 
-/* Structure for environment variables. */
-typedef struct {
-	char name[MAX_ENVNAME + 1];
-	char value[MAX_ENVVALUE + 1];
-} evar_entry;
-evar_entry env_var[MAX_ENVCOUNT];
-int env_count = 0;
-
-/* Stack struct of user thread, see "Exception entry and return" */
-struct user_thread_stack {
-	unsigned int r4;
-	unsigned int r5;
-	unsigned int r6;
-	unsigned int r7;
-	unsigned int r8;
-	unsigned int r9;
-	unsigned int r10;
-	unsigned int fp;
-	unsigned int _lr;	/* Back to system calls or return exception */
-	unsigned int _r7;	/* Backup from isr */
-	unsigned int r0;
-	unsigned int r1;
-	unsigned int r2;
-	unsigned int r3;
-	unsigned int ip;
-	unsigned int lr;	/* Back to user thread code */
-	unsigned int pc;
-	unsigned int xpsr;
-	unsigned int stack[STACK_SIZE - 18];
-};
-
-/* Task Control Block */
-struct task_control_block {
-    struct user_thread_stack *stack;
-    int pid;
-    int status;
-    int priority;
-    struct task_control_block **prev;
-    struct task_control_block  *next;
-};
 struct task_control_block tasks[TASK_LIMIT];
 
 /* 
---
+++ kernel.h
@@ -0,0 +1,113 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#define MAX_CMDNAME 19
+#define MAX_ARGC 19
+#define MAX_CMDHELP 1023
+#define HISTORY_COUNT 20
+#define CMDBUF_SIZE 100
+#define MAX_ENVCOUNT 30
+#define MAX_ENVNAME 15
+#define MAX_ENVVALUE 127
+
+
+#define STACK_SIZE 512 /* Size of task stacks in words */
+#define TASK_LIMIT 8  /* Max number of tasks we can handle */
+#define PIPE_BUF   64 /* Size of largest atomic pipe message */
+#define PATH_MAX   32 /* Longest absolute path */
+#define PIPE_LIMIT (TASK_LIMIT * 2)
+
+#define PATHSERVER_FD (TASK_LIMIT + 3) 
+	/* File descriptor of pipe to pathserver */
+
+#define PRIORITY_DEFAULT 20
+#define PRIORITY_LIMIT (PRIORITY_DEFAULT * 2 - 1)
+
+#define TASK_READY      0
+#define TASK_WAIT_READ  1
+#define TASK_WAIT_WRITE 2
+#define TASK_WAIT_INTR  3
+#define TASK_WAIT_TIME  4
+
+#define S_IFIFO 1
+#define S_IMSGQ 2
+
+#define O_CREAT 4
+
+typedef void (*task_entry_t)();
+
+/* Enumeration for command types. */
+typedef enum {
+	CMD_ECHO = 0,
+	CMD_EXPORT,
+	CMD_HELP,
+	CMD_HISTORY,
+	CMD_MAN,
+	CMD_PS,
+	CMD_EXEC,
+	CMD_COUNT
+} CMD_TYPE;
+
+/* Structure for environment variables. */
+typedef struct {
+	char name[MAX_ENVNAME + 1];
+	char value[MAX_ENVVALUE + 1];
+} evar_entry;
+
+/* Command handlers. */
+void export_envvar(int argc, char *argv[]);
+void show_echo(int argc, char *argv[]);
+void show_cmd_info(int argc, char *argv[]);
+void show_task_info(int argc, char *argv[]);
+void show_man_page(int argc, char *argv[]);
+void show_history(int argc, char *argv[]);
+void create_process(int argc, char *argv[]);
+
+/* Structure for command handler. */
+typedef struct {
+	char cmd[MAX_CMDNAME + 1];
+	void (*func)(int, char**);
+	char description[MAX_CMDHELP + 1];
+} hcmd_entry;
+
+/* function prototypes */
+void *memcpy(void *dest, const void *src, size_t n);
+void check_keyword();
+void find_events();
+void itoa(int n, char *dst, int base);
+
+
+
+/* Stack struct of user thread, see "Exception entry and return" */
+struct user_thread_stack {
+	unsigned int r4;
+	unsigned int r5;
+	unsigned int r6;
+	unsigned int r7;
+	unsigned int r8;
+	unsigned int r9;
+	unsigned int r10;
+	unsigned int fp;
+	unsigned int _lr;	/* Back to system calls or return exception */
+	unsigned int _r7;	/* Backup from isr */
+	unsigned int r0;
+	unsigned int r1;
+	unsigned int r2;
+	unsigned int r3;
+	unsigned int ip;
+	unsigned int lr;	/* Back to user thread code */
+	unsigned int pc;
+	unsigned int xpsr;
+	unsigned int stack[STACK_SIZE - 18];
+};
+
+/* Task Control Block */
+struct task_control_block {
+    struct user_thread_stack *stack;
+    int pid;
+    int status;
+    int priority;
+    struct task_control_block **prev;
+    struct task_control_block  *next;
+};
+#endif
